87cc50542ca1c729f9c4fbb3ab3981f0120728b9
geoknee <georgeknee@googlemail.com>
2025-09-18 11:28:49 +0100

    commit script to generate specs
    This anticipates https://github.com/ethereum-optimism/specs/pull/673

---
+++ scripts/upgrades/gen_jovian_upgrade_tx_specs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+GIT_COMMIT_HASH=7daaabbf2c2cce892aa171bc7e1331ad31bcc8ca
+FROM_ADDRESS_NONCE=0
+FROM_ADDRESS=0x4210000000000000000000000000000000000006
+FORK_NAME=Jovian
+
+# Array of contract names and their corresponding proxy addresses
+declare -a contracts=(
+    "L1Block:0x4200000000000000000000000000000000000015"
+)
+
+inc_hex() {
+  # $1 : hex string, with or without 0x
+  local hex="${1#0x}"      # remove leading “0x” if it’s there
+  # perform the add via bc
+  local next
+  next=$(printf 'obase=16; ibase=16; %s + 1\n' "$hex" | bc)
+  # print with 0x
+  printf '0x%s\n' "$next"
+}
+
+for contract_pair in "${contracts[@]}"; do
+    IFS=':' read -r contract_name proxy_address <<< "$contract_pair"
+    ./scripts/run_gen_predeploy_docs.sh \
+        --optimism-repo-path ../optimism \
+        --fork-name $FORK_NAME \
+        --contract-name "$contract_name" \
+        --from-address $FROM_ADDRESS \
+        --from-address-nonce $FROM_ADDRESS_NONCE \
+        --git-commit-hash $GIT_COMMIT_HASH \
+        --eth-rpc-url https://optimism.rpc.subquery.network/public \
+        --proxy-address "$proxy_address" \
+        --copy-contract-bytecode true
+    # increment the from address itself by 1 in hex here
+    FROM_ADDRESS=$(inc_hex "$FROM_ADDRESS")
+done
